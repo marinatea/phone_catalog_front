e8924077f543e0b3cf8647c607d1cc12dfade2e6
Justin <thedarkvibe1@gmail.com.com>
2024-05-20 16:32:09 +0200

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
 
-export const App = () => <div className="App"></div>;
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="*" element={<ErrorPage />} /> */}
+      </Route>
+    </Routes>
+  </Router>
+);
