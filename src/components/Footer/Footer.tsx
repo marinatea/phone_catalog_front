280e1c6ae0bc4f7c7a2ec07432b0d47309c92604
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-05-21 13:33:37 +0200

    Implemented modules into Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,32 @@
 import { Link } from 'react-router-dom';
-import './Footer.module.scss';
+import styles from './Footer.module.scss';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <Link to="/" className="footer__logo">
+    <footer className={styles.footer}>
+      <Link to="/" className={styles.footer__logo}>
         <img src="/icons/logo.png" alt="logo" />
       </Link>
 
-      <nav className="footer-nav footer__nav">
-        <a href="https://github.com/" className="footer-nav__item">
+      <nav className={`${styles['footer-nav']} ${styles.footer__nav}`}>
+        <a href="https://github.com/" className={styles['footer-nav__item']}>
           Github
         </a>
-        <a href="/" className="footer-nav__item">
+        <a href="/" className={styles['footer-nav__item']}>
           Contacts
         </a>
-        <a href="/" className="footer-nav__item">
+        <a href="/" className={styles['footer-nav__item']}>
           Rights
         </a>
       </nav>
 
-      <div className="top-link">
-        <p className="top-link__text footer__top-link">Back to top</p>
+      <div className={styles['top-link']}>
+        <p
+          className={styles['top-link__text']}
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          Back to top
+        </p>
         <button>
           <img src="/icons/footer_button.png" alt="button" />
         </button>
