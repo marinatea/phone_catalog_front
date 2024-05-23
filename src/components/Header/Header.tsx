b9d75d92adc9335ee69849a407fb7d057ad293d8
Maryna Karpova <mari.artarea@gmail.com>
2024-05-23 10:12:45 +0200

    header2

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
+import Icon from '../Icon/Icon';
+import { Icons } from '../../types';
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <img className={styles.divLogo} src={'/img/Logo.png'} alt="logo" />
+      <nav className={styles.navbar}>
+        <ul className={styles.navlinks}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/phones">Phones</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tablets">Tablets</NavLink>
+          </li>
+          <li>
+            <NavLink to="/accessories">Accessories</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.icons}>
+        <Icon iconId={Icons.HEART} className={styles.heart} />
+        <Icon iconId={Icons.CART} className={styles.cart} />
+        <div className={styles.union}>
+          <Icon iconId={Icons.BURGER} className={styles.burger} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { ProductsProvider } from '../../context/ProductsContext';
+import Header from '../Header/Header';
 
 type Props = {};
 export default function Layout({ }: Props) {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
 
       <ProductsProvider>
         <Outlet />
