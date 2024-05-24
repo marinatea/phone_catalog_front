6f52a2457fe8ca1e6d38646af6e8d2e3ce0b85f1
Paweł Kloc <pawelkloc89@gmail.com>
2024-05-24 13:00:01 +0200

    Cart item UI made

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
 type Props = {};
 import Button from '../Button';
+import CartItem from '../CartItem/CartItem';
 import styles from './CartPage.module.scss';
 
 export default function CartPage({}: Props) {
@@ -7,15 +8,13 @@ export default function CartPage({}: Props) {
     <main className={styles.cartPage}>
       <h1 className={styles.title}>Cart</h1>
       <div className={styles.cardsContainer}>
-        <div className={styles.cardContainer}>Card Placeholder</div>
-        <div className={styles.cardContainer}>Card Placeholder</div>
-        <div className={styles.cardContainer}>Card Placeholder</div>
-        <div className={styles.cardContainer}>Card Placeholder</div>
-        <div className={styles.cardContainer}>Card Placeholder</div>
+        <div className={styles.cardContainer}>
+          <CartItem />
+        </div>
       </div>
       <div className={styles.checkoutContainer}>
         <div className={styles.infoContainer}>
-          <div className={styles.cashInfo}>$2657</div>
+          <div className={styles.cashInfo}>$799</div>
           <div className={styles.itemInfo}>Total for 3 items</div>
         </div>
         <Button
