e53cc2a706a590e30d02039cbea0d002b766adc8
Maryna Karpova <mari.artarea@gmail.com>
2024-05-27 16:59:03 +0200

    header fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,56 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Icon from '../Icon/Icon';
 import { Icons } from '../../types';
-import CartBadge from '../Badge/Badge';
 
-export const Header: React.FC = () => {
+const Header: React.FC = () => {
+  const location = useLocation();
+
   return (
     <header className={styles.header}>
       <img className={styles.divLogo} src={'/img/Logo.png'} alt="logo" />
       <nav className={styles.navbar}>
         <ul className={styles.navlinks}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink
+              to="/"
+              className={location.pathname === '/' ? styles.active : ''}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/phones">Phones</NavLink>
+            <NavLink
+              to="/phones"
+              className={location.pathname === '/phones' ? styles.active : ''}
+            >
+              Phones
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/tablets">Tablets</NavLink>
+            <NavLink
+              to="/tablets"
+              className={location.pathname === '/tablets' ? styles.active : ''}
+            >
+              Tablets
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/accessories">Accessories</NavLink>
+            <NavLink
+              to="/accessories"
+              className={
+                location.pathname === '/accessories' ? styles.active : ''
+              }
+            >
+              Accessories
+            </NavLink>
           </li>
         </ul>
       </nav>
       <div className={styles.icons}>
-        <NavLink to="/favorites">
-          <Icon iconId={Icons.HEART} className={styles.heart} />
-        </NavLink>
-        <NavLink to="/cart">
-          <Icon iconId={Icons.CART} className={styles.cart} />
-          <CartBadge />
-        </NavLink>
+        <Icon iconId={Icons.HEART} className={styles.heart} />
+        <Icon iconId={Icons.CART} className={styles.cart} />
         <div className={styles.union}>
           <Icon iconId={Icons.BURGER} className={styles.burger} />
         </div>
