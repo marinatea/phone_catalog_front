15cbf0fd57414be2ef120bef3188d86bdff68d54
Justin <thedarkvibe1@gmail.com.com>
2024-06-03 15:32:45 +0200

    removed bc

--- src/components/pages/NotFoundPage/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,8 @@
-import Breadcrumbs from '../../generic/Breadcrumbs/Breadcrumbs';
 import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
   return (
     <div className={styles.all}>
-      <Breadcrumbs />
       <h1 className={styles.title}>Page not found</h1>
       <img
         className={styles.photo}
