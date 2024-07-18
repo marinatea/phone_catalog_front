11d0b38f67b832c99ac8a5533c9090cc839b456d
Maryna Karpova <mari.artarea@gmail.com>
2024-07-18 16:21:55 +0200

    Updates

--- src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-// / <reference types="react-scripts" />
-interface ImportMetaEnv {
-  readonly REACT_CLERK_PUBLISHABLE_KEY: string;
-  // add more environment variables here as needed
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
